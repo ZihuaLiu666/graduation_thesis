a327d22fa025451f304fe57048fb50787f075bae
Zihua Liu <31614031+ZihuaLiu666@users.noreply.github.com>
2017-12-11 00:40:18 +0800

    Update plot_Fst.py

--- scripts/Fst/plot_Fst.py
+++ scripts/Fst/plot_Fst.py
@@ -3,6 +3,14 @@
 
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import argparse
+
+###### arguments ######
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--input',metavar='File',dest='input',help='input file',type=open,required=True)
+parser.add_argument('-t','--threshold',metavar='Int',dest='trhd',help='threshold',type=float,required=True)
+args = parser.parse_args()
+###### arguments ######
 
 # Data below is the example
 ## 12      12      3970    1.53940886700739e-05
@@ -11,17 +19,16 @@ import matplotlib.pyplot as plt
 ## 14      14      1850    0.0169749216300928
 
 # load numeric matrix from .txt file
-def loadData(text):
-    inFile = open(text,'r')
+def loadData():
     # define a blank for coming input numeric data
     X = []
-    for line in inFile:
+    for line in args.input:
         trainingSet = line.split()
         #print(trainingSet)
         X.append((int(trainingSet[0]),float(trainingSet[3])))
     #return (X)
     ab = defaultdict(list)
-    for i in range(1,34):
+    for i in range(1,29):
         for n in X:
             if i == n[0]:
                 ab[i].append(n[1])
@@ -42,15 +49,27 @@ def loadData(text):
         if iii == 1:
             #print(range(1,abl[iii]+1))
             #print(ab[iii])
-            plt.scatter(range(1,abl[iii]+1),ab[iii])
+            plt.scatter(range(1,abl[iii]+1),ab[iii],edgecolor = 'none',s = 12,c = (1,0,0))
         elif iii == 2:
             #print(range(l[0]+1,sum(l[0:2])+1))
             #print(ab[iii])
-            plt.scatter(range(l[0]+1,sum(l[0:2])+1),ab[iii])
+            plt.scatter(range(l[0]+1,sum(l[0:2])+1),ab[iii],edgecolor = 'none',s = 12,c = (1,0.502,0))
         else:
             #print(range(sum(l[0:iii-1])+1,sum(l[0:iii])+1))
             #print(ab[iii])
-            plt.scatter(range(sum(l[0:iii-1])+1,sum(l[0:iii])+1),ab[iii])
-    plt.show()
+            cvalue = ['n','n','n',(1,0.749,0),(1,1,0),(0.749,1,0),(0,1,0),(0,1,0.749),(0,1,1),(0,0.749,1),(0,0.512,1),(0,0.39,1),(0.502,0,1),(0.749,0,1),(1,0,1),(1,0,0.749),(1,0,0.502),(1,0,0),(1,0.502,0),(1,0.749,0),(1,1,0),(0.749,1,0),(0,1,0),(0,1,0.749),(0,1,1),(0,0.749,1),(0,0.512,1),(0,0.39,1),(0.502,0,1),(0.749,0,1),(1,0,1),(1,0,0.749),(1,0,0.502)]
+            plt.scatter(range(sum(l[0:iii-1])+1,sum(l[0:iii])+1),ab[iii],edgecolor = 'none',s = 12,c = cvalue[iii])
+    #plt.show()
+    ## plot information
+    plt.ylim(args.trhd,0.55)
+    plt.xlim(0,sum(l))
+    ####
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    ####
+    plt.title('Fst over {} among genome SNPs'.format(args.trhd),fontsize=14)
+    plt.xlabel('position',fontsize=14)
+    plt.ylabel('Fst',fontsize=14)
+    plt.savefig('/Users/funnyboo/Desktop/name.pdf')
 
-##
+loadData()
